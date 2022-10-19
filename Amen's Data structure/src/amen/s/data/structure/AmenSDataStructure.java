2cd2a4835633456ba312748d2a1c00e28ad53a83
Ameen A shalabi <116107964+amenxa@users.noreply.github.com>
2022-10-19 22:01:16 +0300

    Array list modefy

--- Amen's Data structure/src/amen/s/data/structure/AmenSDataStructure.java
+++ Amen's Data structure/src/amen/s/data/structure/AmenSDataStructure.java
@@ -6,7 +6,18 @@ public class AmenSDataStructure {
 
    
     public static void main(String[] args) {
-       
+       ArrayLista <Integer> a = new ArrayLista ();
+       ArrayLista <Integer> a1 = new ArrayLista ();
+       a.add(5);
+       a.add(6);
+       a.add(7);
+       a.add(8);
+       a1.add(5);
+       a1.add(6);
+       a1.add(7);
+       a1.add(8);
+       a.removeAll(a1);
+        System.out.println(a);
     }
     
 }
--- Amen's Data structure/src/amen/s/data/structure/ArrayLista.java
+++ Amen's Data structure/src/amen/s/data/structure/ArrayLista.java
@@ -8,7 +8,7 @@ public class ArrayLista <G>{
     private int capacity ;
     public ArrayLista ()
     {
-        this(0); 
+        this(1); 
     }
     //Expected size
     public ArrayLista (int capacity)
@@ -17,19 +17,107 @@ public class ArrayLista <G>{
     this. capacity =capacity ;
      size = 0 ;
     }
-    void ModifyCapacity ()
+    // helping methods ______________________
+    // modify the capacity
+   private void ModifyCapacity ()
     {
     G arr1[] =(G[])new Object [capacity*=2];
      for (int i = 0; i<size ;i++)
          arr1[i]=arr[i];
      arr = arr1 ;
+   
     }
-    void add (G value)
+   
+   // get the size 
+     public int size ()
+    {
+     return size ;
+    }
+    // to shift to the right or left 
+   private void shift (int index,char c)
+   {
+   if (c=='R')
+   for (int i = size-1;i>=index;i--)
+   arr[i+1]=arr[i];
+   else 
+     for (int i = index ;i<size-1;i++)
+         arr[i]=arr[i+1];
+   }
+   // get elment
+   public G get (int index )
+    {
+    return arr[index];
+    }
+   // set value of elment 
+   public void set (int index,G value)
+   {
+    arr[index]=value ;
+   }
+           
+   // adding _____________________
+   // add at the back 
+   public void add (G value)
     {
      if (size==capacity)
          ModifyCapacity ();
      
      arr[size++]= value ;
     }
+   
+    // add at the index 
+   public void add (int index , G value)
+   {
+     if (size==capacity)
+         ModifyCapacity ();
+     
+   }
+   // add array to this array 
+    public void addAll (ArrayLista a)
+    {
+    for (int i =0 ;i<a.size();i++)     
+        this.add((G) a.get(i));
+    }
+
+   // REMOVE _____________________________________
+    // remove at back 
+    public void remove ()
+    {
+      arr[--size]=null ;
+      
+    }
     
+    // remove at index
+    public void remove (int index)
+    {
+      shift (index,'L');
+      size -- ;
+        System.out.println(index +" removed");
+    }
+    
+    // remove all elments that in array a
+    public void removeAll(ArrayLista  a)
+    {
+     for (int i = 0 ;i<a.size() ;i++)
+     {
+      for (int j = 0 ;j<size;j++)
+      {
+      if (a.get(i).equals(arr[j]))
+         this. remove(j);
+      }
+     }
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        s+='[';
+        for (int i =0 ;i<size;i++)
+            s+=arr[i]+" ";
+        s+=']';
+        return s ;
+    }
+       
+    
+ 
+ 
 }
